44fd19c8d37edf1f53dc9fefb97484b0600f9715
John Ouellet <labboy0276@yahoo.com>
2018-03-15 23:50:59 -0400

    USF#42 Create OG Migration mechanism, fix up classes, etc

--- web/modules/custom/dgreat_migration/src/Plugin/migrate/process/MigrateOgToGroup.php
+++ web/modules/custom/dgreat_migration/src/Plugin/migrate/process/MigrateOgToGroup.php
@@ -8,7 +8,7 @@ use Drupal\migrate\Row;
 use Drupal\migrate\MigrateSkipRowException;
 
 /**
- * Convert a taxonomy tid into a group gid.
+ * Convert a OG gid into a Group gid.
  *
  * @MigrateProcessPlugin(
  *   id = "og_to_group",
@@ -20,8 +20,6 @@ class MigrateOgToGroup extends ProcessPluginBase {
    * {@inheritdoc}
    */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
-
-    var_dump($value);
     // If the tid is not in this array, it doesn't get migrated.
     if (!array_key_exists($value, $this->match())) {
       throw new MigrateSkipRowException('', TRUE);
@@ -32,19 +30,38 @@ class MigrateOgToGroup extends ProcessPluginBase {
   }
 
   /**
-   * Matches an array of tids and gids.
-   * This was matched up in the discovery docs.
+   * Matches an array of og gids and group gids.
    *
    * @return array
-   *   A tid to gid array to chose from.
+   *   A og gid to group gid array to chose from.
    */
   protected function match() {
-    // Format is tid => gid.
+    // Format is og gid => group gid.
     return [
-      '7' => '3',
-      '116' => '4',
-      '111' => '5',
-      '8' => '6',
+      '4522' => '1',
+      '137671' => '2',
+      '4523' => '3',
+      '3908' => '4',
+      '8949' => '5',
+      '136583' => '6',
+      '136584' => '7',
+      '136586' => '8',
+      '136587' => '9',
+      '136585' => '10',
+      '136582' => '11',
+      '136581' => '12',
+      '136580' => '13',
+      '135672' => '14',
+      '135540' => '15',
+      '52539' => '16',
+      '4524' => '17',
+      '4525' => '18',
+      '14790' => '19',
+      '14789' => '20',
+      '8947' => '21',
+      '24976' => '22',
+      '3736' => '23',
+      '4205' => '14',
     ];
   }
 }
