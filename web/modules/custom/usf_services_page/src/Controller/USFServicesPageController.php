17c93c6a98a792d617372a0aad6109adfabf1232
John Ouellet <labboy0276@yahoo.com>
2018-05-16 17:45:46 -0400

    USF#66 Service page tweaks

--- web/modules/custom/usf_services_page/src/Controller/USFServicesPageController.php
+++ web/modules/custom/usf_services_page/src/Controller/USFServicesPageController.php
@@ -2,7 +2,9 @@
 namespace Drupal\usf_services_page\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\user\Entity\Role;
+use Drupal\block\Entity\Block;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 
 /**
@@ -11,18 +13,47 @@ use Drupal\user\Entity\Role;
 class USFServicesPageController extends ControllerBase {
 
   /**
+   * The entity manager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructs a USFServicesPageController object.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity manager service.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager')
+    );
+  }
+
+  /**
    * Returns a simple page.
    *
    * @return array
    *   A simple renderable array.
    */
   public function showServices() {
-    $role_objects[] = Role::loadMultiple();
+    // Renders the services block.
+    $block = Block::load('views_block__services_block_1');
+    $render['services'] = $this->entityTypeManager
+      ->getViewBuilder('block')
+      ->view($block);
 
     return [
       '#theme' => 'usf_services_page',
-      '#test_var' => $role_objects,
-      '#my_user' => $role_objects,
+      '#page' => $render,
     ];
   }
 }
