44dc5031d29d5efceb944a6aaacbdafc68aacf70
Dustin LeBlanc <dustin.m.leblanc@gmail.com>
2018-05-21 16:05:39 -0400

    Stub out webform migration

---
+++ web/modules/custom/dgreat_migration/src/Plugin/migrate/source/Webform.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dustinleblanc
+ * Date: 5/21/18
+ * Time: 11:07 AM
+ */
+
+namespace Drupal\dgreat_migration\Plugin\migrate\source;
+
+use Drupal\webform_migrate\Plugin\migrate\source;
+
+class Webform extends D7Webform
+{
+
+}
