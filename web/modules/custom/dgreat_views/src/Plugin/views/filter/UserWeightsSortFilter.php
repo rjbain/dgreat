483c064b7a3fe9951315d96aa9f585a977198699
John Ouellet <labboy0276@yahoo.com>
2018-05-22 18:23:59 -0400

    USF#63 Start of user weighted sorting

---
+++ web/modules/custom/dgreat_views/src/Plugin/views/filter/UserWeightsSortFilter.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Drupal\dgreat_views\Plugin\views\filter;
+
+use Drupal\views\Plugin\views\filter\FilterPluginBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Database\Query\Condition;
+
+/**
+ * Sorts entities by flagged or unflagged in a view.
+ *
+ * @ViewsFilter("user_weight_filter_sort")
+ */
+class UserWeightsSortFilter extends FilterPluginBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The database connection to which to dump route information.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $db;
+
+  /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected $currentUser;
+
+  /**
+   * Constructs a new LatestRevision.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Database\Connection $db
+   *   The database connection to be used.
+   * @param \Drupal\Core\Path\CurrentPathStack $current_path
+   *   The current path.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, Connection $db, AccountInterface $current_user) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+    $this->db = $db;
+    $this->currentUser = $current_user;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('database'),
+      $container->get('current_user')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function valueForm(&$form, FormStateInterface $form_state) {
+    // Remove the expose checkbox.
+    unset($form["expose_button"]);
+  }
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    $this->ensureMyTable();
+
+    $results = $this->db->select('user_weights', 'u')
+      ->fields('u', ['entity_id'])
+      ->condition('uid', $this->currentUser->id())
+      ->orderBy('weight', 'ASC')
+      ->execute()
+      ->fetchAll();
+
+    foreach ($results as $result) {
+      $weights[] = $result->entity_id;
+    }
+
+
+    $conditions = new Condition('AND');
+    $conditions->condition('nid', $weights, 'IN');
+
+    // Hook up the query.
+    $this->query->addWhere(0, $conditions);
+  }
+
+}
