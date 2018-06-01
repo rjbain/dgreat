33182a42cffae9c855b93eec9d521510f0cdb931
Rob Bain <rjbain@usfca.edu>
2018-06-01 12:39:06 -0700

    selins changes

--- web/themes/custom/myusf/scripts/site/site.js
+++ web/themes/custom/myusf/scripts/site/site.js
@@ -1,1 +1,7 @@
-$(document).ready(function(){$("a.addthis_button > img").replaceWith("<div class='hollow_callout_heading'>Share This Page</div>"),$("a.addthis_button").addClass("hollow_callout_link share addthis_button_more")}),$(document).ready(function(){$("[id^=contentView_]").attr("align","center")});
+$(document).ready(function() {
+          $("a.addthis_button > img").replaceWith("<div class='hollow_callout_heading'>Share This Page</div>");
+          $("a.addthis_button").addClass("hollow_callout_link share addthis_button_more");
+});
+ $(document).ready(function() {
+		 $("[id^=contentView_]").attr("align","center");		 
+   	});
