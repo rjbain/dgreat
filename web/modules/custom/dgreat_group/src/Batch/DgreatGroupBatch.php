572312bbf195bde171bf4c27be8a6f706c76818a
mjuarez2 <mjuarez2@usfca.edu>
2018-07-26 09:39:47 -0700

    Dgreat group mod test

--- web/modules/custom/dgreat_group/src/Batch/DgreatGroupBatch.php
+++ web/modules/custom/dgreat_group/src/Batch/DgreatGroupBatch.php
@@ -29,6 +29,9 @@ class DgreatGroupBatch {
   public static function batchProcess($new_nids, $removed_nids, $group, $member, &$context) {
     $user = $member->getUser();
 
+    if ($user === NULL) {
+      return;
+    }
     // Exit if we picked up user 0.
     if ($user->id() == 0) {
       return;
