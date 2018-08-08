c9cbadb1520ba95f350beddf84652b678391ac3c
Rob Bain <rjbain@usfca.edu>
2018-08-08 12:29:15 -0700

    fixing addThis.js

--- web/themes/custom/myusf/scripts/src/addthis.js
+++ web/themes/custom/myusf/scripts/src/addthis.js
@@ -1,4 +1,7 @@
-$(document).ready(function() {
+(function ($, Drupal) {
+    Drupal.behaviors.addThis = {
+        attach: function (context, settings) {
           $("a.addthis_button > img").replaceWith("<div class='hollow_callout_heading'>Share This Page</div>");
           $("a.addthis_button").addClass("hollow_callout_link share addthis_button_more");
-});
+        };
+})(jQuery, Drupal);
