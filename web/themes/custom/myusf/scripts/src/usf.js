be26c3d95ac0a4f7117c3b4d5bcad89e39f32edd
Rob Bain <rjbain@usfca.edu>
2018-08-08 10:27:59 -0700

    adding usf.js

---
+++ web/themes/custom/myusf/scripts/src/usf.js
@@ -0,0 +1,13 @@
+$(".nav-item").mouseleave(function(){
+    this.blur();
+});
+$(".carousel-control-next").mouseleave(function(){
+    this.blur();
+});
+$(".carousel-control-prev").mouseleave(function(){
+    this.blur();
+});
+
+$(".header_search").mouseleave(function(){
+    this.blur();
+});
