99dc271fe1f53923dc7795623901569c92b1b13b
Rob Bain <rjbain@usfca.edu>
2018-08-08 12:14:42 -0700

    global.js

--- web/themes/custom/myusf/scripts/src/global.js
+++ web/themes/custom/myusf/scripts/src/global.js
@@ -1,15 +1,19 @@
-$(document).ready(function() {
-    $(".nav-item").mouseleave(function(){
-        this.blur();
-    });
-    $(".carousel-control-next").mouseleave(function(){
-        this.blur();
-    });
-    $(".carousel-control-prev").mouseleave(function(){
-        this.blur();
-    });
+(function ($, Drupal) {
+    Drupal.behaviors.fixHover = {
+        attach: function (context, settings) {
+            $(".nav-item").mouseleave(function(){
+                this.blur();
+            });
+            $(".carousel-control-next").mouseleave(function(){
+                this.blur();
+            });
+            $(".carousel-control-prev").mouseleave(function(){
+                this.blur();
+            });
 
-    $(".header_search").mouseleave(function(){
-        this.blur();
-    });
-});
+            $(".header_search").mouseleave(function(){
+                this.blur();
+            });
+        }
+    };
+})(jQuery, Drupal);
