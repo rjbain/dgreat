a80f18a4cd14249759fac0c5c59087d33190dafa
Dustin LeBlanc <dustin.m.leblanc@gmail.com>
2018-08-17 13:38:50 -0400

    #152: Provide form to update the current student list for surveys

--- web/modules/custom/dgreat_student_surveys/src/Form/StudentSurveySettingsForm.php
+++ web/modules/custom/dgreat_student_surveys/src/Form/StudentSurveySettingsForm.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\dgreat_student_surveys\Form;
 
+use Drupal\Core\Database\Database;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\file\Entity\File;
@@ -29,7 +30,7 @@ class StudentSurveySettingsForm extends FormBase {
       '#title' => $this->t('Current Student XML File'),
       '#description' => $this->t('An XML File from Banner that indicates which students should see forms'),
       '#upload_validators' => [
-        'file_validate_extensions' => ['xml']
+        'file_validate_extensions' => ['xml'],
       ],
       '#upload_location' => 'private://student_surveys/',
       '#weight' => '0',
@@ -47,25 +48,42 @@ class StudentSurveySettingsForm extends FormBase {
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
     if ($form_state->getValue('current_student_xml_file') == NULL) {
-      $form_state->setErrorByName('current_student_xml_file', $this->t('File.'));
+      $form_state->setErrorByName('current_student_xml_file',
+        $this->t('File.'));
     }
   }
 
   /**
    * {@inheritdoc}
+   * @throws \Exception
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $fid = $form_state->getValue('current_student_xml_file');
-    if (!empty($fid)) {
-      $file = File::load($fid);
-      if (null !== $file) {
-        $file->setPermanent();
-        $file->save();
+    $fids = $form_state->getValue('current_student_xml_file');
+    if (!empty($fids)) {
+      // We only allow a single upload
+      $file = File::load($fids[0]);
+      if (NULL !== $file) {
+        $data = simplexml_load_string(file_get_contents($file->getFileUri()));
+
+        $conn = Database::getConnection();
+
+        // Truncate the table
+        $conn->truncate('current_survey_students')->execute();
+
+        // Loop over XML nodes and add user names to database.
+        foreach ($data->DATA_RECORD as $record) {
+          try {
+            $conn->insert('current_survey_students')->fields([
+              'username' => $record->USERNAME,
+            ])->execute();
+          } catch (\Exception $e) {
+            \Drupal::logger('dgreat_student_surveys')
+                   ->error('Error in update of current_survey_students table');
+          }
+        }
       }
     }
-
     \Drupal::service('messenger')->addMessage('Current Student File Updated');
-
   }
-
 }
+
