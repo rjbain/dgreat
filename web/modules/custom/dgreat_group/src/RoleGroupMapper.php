71e73730c872a0cef4dac2c8e93c82491976a51a
Dustin LeBlanc <dustin.m.leblanc@gmail.com>
2018-09-18 17:56:51 -0400

    #365: Don’t alter user fields when user logs in

--- web/modules/custom/dgreat_group/src/RoleGroupMapper.php
+++ web/modules/custom/dgreat_group/src/RoleGroupMapper.php
@@ -65,16 +65,6 @@ class RoleGroupMapper {
    * @throws \Drupal\Core\Entity\EntityStorageException
    */
   public static function revokeGroupAccess(User $user, $group_id) {
-    // Build the new list of groups the user has access to.
-    $new_groups = collect($user->get('field_user_group')->getValue())
-      ->filter(function ($group) use ($group_id) {
-        return $group['target_id'] !== $group_id;
-      })->toArray();
-
-    $user->set('field_user_group', $new_groups);
-    $user->save();
-
-    //remove the user from the group.
     Group::load($group_id)->removeMember($user);
     return $user;
   }
