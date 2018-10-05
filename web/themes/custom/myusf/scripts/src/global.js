c47f01a926b2c71bc66067f5146529984ca3ee1f
Dustin LeBlanc <dustin.m.leblanc@gmail.com>
2018-10-05 09:02:04 -0400

    #154: get rid of extra jQuery wrapper

--- web/themes/custom/myusf/scripts/src/global.js
+++ web/themes/custom/myusf/scripts/src/global.js
@@ -25,9 +25,7 @@
     Drupal.behaviors.surveyModal = {
         attach: function (context, settings) {
             // Using once() to apply the myCustomBehaviour effect when you want to run just one function.
-            (function ($) {
-                $('#studentSurveyModal').once('surveyModal').modal('show');
-            })(jQuery);
+            $('#studentSurveyModal').once('surveyModal').modal('show');
         }
     };
 })(jQuery, Drupal);
