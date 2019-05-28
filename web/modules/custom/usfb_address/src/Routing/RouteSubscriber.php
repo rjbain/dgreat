791400211dee58f39ea0b6918a836c2e987316b4
Rob Bain <rjbain@usfca.edu>
2019-05-28 14:57:32 -0700

    creating custom route permissions

---
+++ web/modules/custom/usfb_address/src/Routing/RouteSubscriber.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\usfb_address\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Listens to the dynamic route events.
+ */
+class RouteSubscriber extends RouteSubscriberBase {
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function alterRoutes(RouteCollection $collection) {
+        // Define custom access for '/user/{user}'
+        if ($route = $collection->get('entity.user.canonical')) {
+            $route->setRequirement('_custom_access', '\Drupal\your_module\Access\YourCustomAccessCheck::access');
+        }
+    }
+
+}
