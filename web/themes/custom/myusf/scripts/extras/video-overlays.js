54304a4a38962284114b30513cc8e909fbee2be8
Rob Bain <rjbain@usfca.edu>
2020-04-28 11:27:07 -0700

    video component

--- web/themes/custom/myusf/scripts/extras/video-overlays.js
+++ web/themes/custom/myusf/scripts/extras/video-overlays.js
@@ -1,14 +1,16 @@
 // Get and autoplay youtube video from datatag.
-function autoPlayYouTubeModal() { console.log("hi");
+function autoPlayYouTubeModal() {
     var trigger = $("body").find('[data-toggle="modal"]');
     trigger.click(function() {
         var theModal = $(this).data( "target" ),
-            videoSRC = $(this).attr( "data-theVideo" ),
-            videoSRCauto = videoSRC+"?autoplay=1" ;
-        console.log("The value of videoSRC is: " + videoSRC);
-        $(theModal+' iframe').attr('src', videoSRCauto);
+            // videoSRCauto = videoSRC+"?autoplay=1"
+            videoSRC = $(this).attr( "data-theVideo" );
+        // first make sure the src is empty
+        $(theModal+' iframe').attr('src', "");
+        $(theModal+' iframe').attr('src', videoSRC);
         $(theModal+' button.close').click(function () {
-            $(theModal+' iframe').attr('src', videoSRC);
+            $(theModal+' iframe').attr('src', '');
+            // $(theModal+' iframe').attr('src', videoSRC);
         });
     });
 }
