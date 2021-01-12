c7572d857568e42bf0366f57c000c017bd635d0d
Rob Bain <rjbain@usfca.edu>
2021-01-12 13:52:09 -0800

    re-adding DRUPAL_ROOT

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -19,7 +19,7 @@ include __DIR__ . "/settings.pantheon.php";
 /**
  * Place the config directory outside of the Drupal root.
  */
-$settings['config_sync_directory'] = '/config';
+$settings['config_sync_directory'] = dirname(DRUPAL_ROOT) . '/config';
 
 /**
  * Always install the 'standard' profile to stop the installer from
