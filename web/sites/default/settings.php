6e052486b1b6005cabc0e96aa2bdb2f2607b1d80
Michael Juarez <mjuarez@coscient.com>
2023-05-16 14:53:41 -0700

    Adding line to settings for temp files in feed imports

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -129,3 +129,9 @@ if (isset($_SERVER['PANTHEON_ENVIRONMENT']) && php_sapi_name() != 'cli' && $_SER
     exit();
 }
 
+// Temp file setting for containerized env
+// Make sure to FTP and add directory to file structure in test and live
+if (($_ENV['PANTHEON_ENVIRONMENT'] === 'live') || ($_ENV['PANTHEON_ENVIRONMENT'] === 'test'))  {
+  $config['system.file']['path']['temporary'] = __DIR__ . '/files/tmp';
+  $settings['file_temp_path'] = __DIR__ . '/files/tmp';
+}
