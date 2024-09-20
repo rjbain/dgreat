c55d5a67a2a1cfd2ff5784c3ae6a8b532bdbf46c
Rob Bain <rjbain@usfca.edu>
2024-09-20 16:43:26 -0700

    adding video focus

--- web/themes/custom/myusf/scripts/extras/video-overlays.js
+++ web/themes/custom/myusf/scripts/extras/video-overlays.js
@@ -1,22 +1,26 @@
 // Get and autoplay youtube video from datatag.
 function autoPlayYouTubeModal() {
-    var trigger = $("body").find('[data-toggle="modal"]');
-    trigger.click(function() {
-        var theModal = $(this).data( "target" ),
-            videoSRC = $(this).attr( "data-theVideo" ),
-            videoSRCauto = videoSRC+"?autoplay=1";
-        // first make sure the src is empty
-        $(theModal+' iframe').attr('src', "");
-        $(theModal+' iframe').attr('src', videoSRCauto).focus(function () {
-        console.log("focusing on iframe");
-      });
-        $(theModal+' button.close, #videoModal').click(function () {
-            $(theModal+' iframe').attr('src', '');
-            // $(theModal+' iframe').attr('src', videoSRC);
-        });
+  var trigger = $("body").find('[data-toggle="modal"]');
+  trigger.click(function() {
+    let theModal = $(this).data( "target" ),
+      videoSRC = $(this).attr( "data-theVideo" ),
+      videoSRCauto = videoSRC+"?autoplay=1";
+    let vidIframe = $(theModal+' iframe');
+    // first make sure the src is empty
+    vidIframe.attr('src', "");
+    vidIframe.attr('src', videoSRCauto);
+
+    vidIframe.load(function() {
+      $(theModal+' iframe').focus();
     });
+
+    $(theModal+' button.close, #videoModal').click(function () {
+      $(theModal+' iframe').attr('src', '');
+      // $(theModal+' iframe').attr('src', videoSRC);
+    });
+  });
 }
 
 $(document).ready(function(){
-    autoPlayYouTubeModal();
+  autoPlayYouTubeModal();
 });
