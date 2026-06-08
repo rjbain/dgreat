3c15e1a87759e489df887a89b610e74fd565bb44
Michael Juarez <mjuarez2@usfca.edu>
2026-06-08 14:00:44 -0700

    Pantheon: rotate service container cache per deploy

--- web/sites/default/settings.pantheon.php
+++ web/sites/default/settings.pantheon.php
@@ -159,12 +159,16 @@ if (isset($_ENV['PANTHEON_ENVIRONMENT'])) {
  * to see updated results after a code deploy.
  */
 if (isset($_ENV['PANTHEON_ROLLING_TMP']) && isset($_ENV['PANTHEON_DEPLOYMENT_IDENTIFIER'])) {
+  // Force Drupal to generate a fresh compiled service container on every
+  // Pantheon deploy. This avoids reusing stale cached container definitions
+  // across major-version upgrades during automated Drush operations.
+  $settings['deployment_identifier'] = $_ENV['PANTHEON_DEPLOYMENT_IDENTIFIER'];
+
   // Relocate the compiled twig files to <binding-dir>/tmp/ROLLING/twig.
   // The location of ROLLING will change with every deploy.
   $settings['php_storage']['twig']['directory'] = $_ENV['PANTHEON_ROLLING_TMP'];
   // Ensure that the compiled twig templates will be rebuilt whenever the
   // deployment identifier changes.  Note that a cache rebuild is also necessary.
-  $settings['deployment_identifier'] = $_ENV['PANTHEON_DEPLOYMENT_IDENTIFIER'];
   $settings['php_storage']['twig']['secret'] = $_ENV['DRUPAL_HASH_SALT'] . $settings['deployment_identifier'];
 }
 
